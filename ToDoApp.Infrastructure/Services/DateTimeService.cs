2a4a95bb3781e109dc0913d2bfc6e251ae736b13
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-19 18:43:02 +0500

    save

--- ToDoApp.Infrastructure/Services/DateTimeService.cs
+++ ToDoApp.Infrastructure/Services/DateTimeService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoApp.Application.Commons.Interfaces;
 
 namespace ToDoApp.Infrastructure.Services
 {
-    internal class DateTimeService
+    public class DateTimeService : IDateTime
     {
+        public DateTime Now => DateTime.UtcNow;
     }
 }
