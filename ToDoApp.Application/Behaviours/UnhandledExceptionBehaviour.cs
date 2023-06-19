b6dcb12c5f490ba3850d5e497f9a04045af041cf
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-19 16:20:00 +0500

    save

---
+++ ToDoApp.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Serilog;
+
+namespace ToDoApp.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+
+                Log.Error(ex, $"HRMS Request: Unhandled Exception for Request {requestName} {request}\n");
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ToDoApp.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace ToDoApp.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            : base("One or more validation failures have occurred.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(string name, string message)
+        {
+            Errors = new Dictionary<string, string[]>
+            {
+                { name, new string[] { message } }
+            };
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key,
+                              failureGroup => failureGroup.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; set; }
+    }
+}
