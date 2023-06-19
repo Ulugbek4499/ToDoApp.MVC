2c3c59ba82cfab286cb13c0b31c9ba5cb3fe173b
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-19 17:43:00 +0500

    save

--- ToDoApp.Application/UseCases/ToDoItems/Queries/GetToDoItems/GetToDoItemsQuery.cs
+++ ToDoApp.Application/UseCases/ToDoItems/Queries/GetToDoItems/GetToDoItemsQuery.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Application.Commons.Interfaces;
+using ToDoApp.Application.Commons.Models;
+using ToDoApp.Domain.Entities;
 
 namespace ToDoApp.Application.UseCases.ToDoItems.Queries.GetToDoItems
 {
-    internal class GetToDoItemsQuery
+    public record GetToDoItemsQuery : IRequest<ToDoItemDto[]>;
+
+    public class GetToDoItemsQueryHandler : IRequestHandler<GetToDoItemsQuery, ToDoItemDto[]>
     {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetToDoItemsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ToDoItemDto[]> Handle(GetToDoItemsQuery request, CancellationToken cancellationToken)
+        {
+            ToDoItem[] ToDoItems = await _context.ToDoItems.ToArrayAsync();
+
+            return _mapper.Map<ToDoItemDto[]>(ToDoItems);
+        }
     }
 }
