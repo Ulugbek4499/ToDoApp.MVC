f9081eba39416ed0edce3681b0ac302de1579d8b
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-19 17:10:41 +0500

    save

--- ToDoApp.Application/UseCases/ToDoLists/Queries/GetToDoList/GetToDoListQuery.cs
+++ ToDoApp.Application/UseCases/ToDoLists/Queries/GetToDoList/GetToDoListQuery.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using ToDoApp.Application.Commons.Exceptions;
+using ToDoApp.Application.Commons.Interfaces;
+using ToDoApp.Application.Commons.Models;
+using ToDoApp.Domain.Entities;
 
 namespace ToDoApp.Application.UseCases.ToDoLists.Queries.GetToDoList
 {
-    internal class GetToDoListQuery
+    public record GetToDoListQuery(Guid ToDoListId) : IRequest<ToDoListDto>;
+
+    public class GetToDoListQueryHandler : IRequestHandler<GetToDoListQuery, ToDoListDto>
     {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetToDoListQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ToDoListDto> Handle(GetToDoListQuery request, CancellationToken cancellationToken)
+        {
+            ToDoList maybeToDoList = await _context.ToDoLists
+                .FindAsync(new object[] { request.ToDoListId });
+
+            ValidateToDoListIsNotNull(request, maybeToDoList);
+
+            return _mapper.Map<ToDoListDto>(maybeToDoList);
+        }
+
+        private static void ValidateToDoListIsNotNull(GetToDoListQuery request, ToDoList maybeToDoList)
+        {
+            if (maybeToDoList == null)
+            {
+                throw new NotFoundException(nameof(ToDoList), request.ToDoListId);
+            }
+        }
     }
 }
--- ToDoApp.Application/UseCases/ToDoLists/Queries/GetToDoLists/GetToDoListsQuery.cs
+++ ToDoApp.Application/UseCases/ToDoLists/Queries/GetToDoLists/GetToDoListsQuery.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Application.Commons.Interfaces;
+using ToDoApp.Application.Commons.Models;
+using ToDoApp.Domain.Entities;
 
 namespace ToDoApp.Application.UseCases.ToDoLists.Queries.GetToDoLists
 {
-    internal class GetToDoListsQuery
+    public record GetToDoListsQuery : IRequest<ToDoListDto[]>;
+
+    public class GetToDoListsQueryHandler : IRequestHandler<GetToDoListsQuery, ToDoListDto[]>
     {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetToDoListsQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ToDoListDto[]> Handle(GetToDoListsQuery request, CancellationToken cancellationToken)
+        {
+            ToDoList[] ToDoLists = await _context.ToDoLists.ToArrayAsync();
+
+            return _mapper.Map<ToDoListDto[]>(ToDoLists);
+        }
     }
 }
