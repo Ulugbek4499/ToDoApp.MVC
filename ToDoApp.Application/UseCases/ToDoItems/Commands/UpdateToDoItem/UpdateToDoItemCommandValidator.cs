ed42ac87a482129640ff94f5e3ed3d6228d5b241
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-19 17:42:00 +0500

    #saves

---
+++ ToDoApp.Application/UseCases/ToDoItems/Commands/UpdateToDoItem/UpdateToDoItemCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace ToDoApp.Application.UseCases.ToDoItems.Commands.UpdateToDoItem
+{
+    public class UpdateToDoItemCommandValidator:AbstractValidator<UpdateToDoItemCommand>
+    {
+        public UpdateToDoItemCommandValidator()
+        {
+            RuleFor(d => d.Id)
+               .NotEmpty()
+               .NotEqual((Guid)default)
+               .WithMessage("ToDoItem Id is required.");
+
+            RuleFor(d => d.Title).NotEmpty()
+             .MaximumLength(50)
+             .WithMessage("Title is required");
+
+            RuleFor(d => d.Description).NotEmpty()
+              .MaximumLength(50)
+              .WithMessage("Description is required");
+
+            RuleFor(d => d.DueDate)
+               .NotEqual((DateTime)default)
+               .WithMessage("Due Date is required.");
+
+            RuleFor(d => d.ToDoListId)
+                .NotEmpty()
+                .NotEqual((Guid)default)
+                .WithMessage("ToDoList Id is required.");
+        }
+    }
+}
--- ToDoApp.Application/UseCases/ToDoItems/Queries/GetToDoItem/GetToDoItemQuery.cs
+++ ToDoApp.Application/UseCases/ToDoItems/Queries/GetToDoItem/GetToDoItemQuery.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using ToDoApp.Application.Commons.Exceptions;
+using ToDoApp.Application.Commons.Interfaces;
+using ToDoApp.Application.Commons.Models;
+using ToDoApp.Domain.Entities;
 
 namespace ToDoApp.Application.UseCases.ToDoItems.Queries.GetToDoItem
 {
-    internal class GetToDoItemQuery
+    public record GetToDoItemQuery(Guid ToDoItemId) : IRequest<ToDoItemDto>;
+
+    public class GetToDoItemQueryHandler : IRequestHandler<GetToDoItemQuery, ToDoItemDto>
     {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetToDoItemQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ToDoItemDto> Handle(GetToDoItemQuery request, CancellationToken cancellationToken)
+        {
+            ToDoItem maybeToDoItem = await
+                _context.ToDoItems.FindAsync(new object[] { request.ToDoItemId });
+
+            ValidateToDoItemIsNotNull(request, maybeToDoItem);
+
+            return _mapper.Map<ToDoItemDto>(maybeToDoItem);
+        }
+
+        private void ValidateToDoItemIsNotNull(GetToDoItemQuery request, ToDoItem? maybeToDoItem)
+        {
+            if (maybeToDoItem is null)
+            {
+                throw new NotFoundException(nameof(ToDoItem), request.ToDoItemId);
+            }
+        }
     }
 }
