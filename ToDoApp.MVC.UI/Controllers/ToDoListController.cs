3cd521d4bf6faee50320265f6fa2d8bb3b5bc977
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-06-20 19:23:57 +0500

    save

--- ToDoApp.MVC.UI/Controllers/ToDoListController.cs
+++ ToDoApp.MVC.UI/Controllers/ToDoListController.cs
@@ -1,15 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
 using ToDoApp.Application.Commons.Models;
+using ToDoApp.Application.UseCases.ToDoLists.Commands.CreateToDoList;
+using ToDoApp.Application.UseCases.ToDoLists.Commands.DeleteToDoList;
+using ToDoApp.Application.UseCases.ToDoLists.Commands.UpdateToDoList;
 using ToDoApp.Application.UseCases.ToDoLists.Queries.GetToDoList;
 using ToDoApp.Application.UseCases.ToDoLists.Queries.GetToDoLists;
-using ToDoApp.Domain.Entities;
 
 namespace ToDoApp.MVC.Controllers
 {
     public class ToDoListController : ApiBaseController
     {
-        [HttpGet]
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> CreateToDoList()
+        {
+            return View();
+        }
+
+        [HttpPost("[action]")]
+        public async ValueTask<IActionResult> CreateToDoList([FromForm] CreateToDoListCommand ToDoList)
+        {
+            await Mediator.Send(ToDoList);
+
+            return RedirectToAction("GetAllToDoLists");
+        }
+
+        [HttpGet("[action]")]
         public async ValueTask<IActionResult> GetAllToDoLists()
         {
            ToDoListDto[] toDoLists =await Mediator.Send(new GetToDoListsQuery());
@@ -17,13 +32,32 @@ namespace ToDoApp.MVC.Controllers
             return View(toDoLists);
         }
 
-        [HttpGet]
+        [HttpGet("[action]")]
         public async ValueTask<IActionResult> ViewToDoList(Guid id)
         {
             var toDoList = await Mediator.Send(new GetToDoListQuery(id));
-
+        
             return View("ViewToDoList", toDoList);
         }
 
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> UpdateToDoList(ToDoListDto toDoListDto)
+        {
+          return  View(toDoListDto);
+        }
+
+        [HttpPost("[action]")]
+        public async ValueTask<IActionResult> UpdateToDoList([FromForm] UpdateToDoListCommand toDoList)
+        {
+            await Mediator.Send(toDoList);
+            return RedirectToAction("GetAllToDoLists");
+        }
+
+        public async ValueTask<IActionResult> DeleteToDoList(Guid Id)
+        {
+            await Mediator.Send(new DeleteToDoListCommand(Id));
+
+            return RedirectToAction("GetAllToDoLists");
+        }
     }
 }
